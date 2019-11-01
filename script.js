47aafd07f17c4a61b3d7552d0917c7508fc5f16f
Antonio Britto <antonioabritto@gmail.com>
2019-10-31 23:06:51 -0300

    eslint

--- script.js
+++ script.js
@@ -6,30 +6,22 @@ assert.equal(!!NaN, false);
 assert.equal(!!0, false);
 assert.equal(!!'', false);
 assert.equal(!!false, false, 'this is pure nonsense');
-
 //------------------------------
+const anyGreaterThan10 = input => input.some(value => value > 10);
 
-const anyGreaterThan10 = (input) => {
-  return input.some((value) => value > 10);
-}
+const allSameType = input => input.every(value => typeof (value) === typeof (input[0]));
 
-const allSameType = (input) => {
-  return input.every((value) => typeof (value) == typeof (input[0]));
-}
+const removeNonVowels = word => word.replace(/[^aeiou]/gi, '');
 
-const removeNonVowels = (word) => word.replace(/[^aeiou]/gi, '');
+const onlyVowels = input => input.map(removeNonVowels);
 
-const onlyVowels = (input) => input.map(removeNonVowels);
+const wordUniqueCharacters = word => word.split('').filter((item, index, input) => input.indexOf(item) === index);
 
-const wordUniqueCharacters = (word) => word.split('').filter((item, index, input) => input.indexOf(item) === index);
-
-const allSameVowels = (input) => {
-  return input.filter((word) => {
-    let vowels = removeNonVowels(word);
-    let uniqueVowels = wordUniqueCharacters(vowels);
-    return uniqueVowels.length == 1;
-  })
-}
+const allSameVowels = (input) => input.filter((word) => {
+  const vowels = removeNonVowels(word);
+  const uniqueVowels = wordUniqueCharacters(vowels);
+  return uniqueVowels.length === 1;
+});
 //------------------------------
 let goodInput = [8, 9, 10, 11];
 let badInput = [1, 2, 3, 4];
