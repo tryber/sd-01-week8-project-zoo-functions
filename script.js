84113935a257daac80bc26d885d2bd4123177560
Antonio Britto <antonioabritto@gmail.com>
2019-10-31 22:58:45 -0300

    why fail

--- script.js
+++ script.js
@@ -1,67 +1,60 @@
-const assert = require('assert')
+const assert = require('assert');
 
-assert.equal(!!undefined, false)
-assert.equal(!!null, false)
-assert.equal(!!NaN, false)
-assert.equal(!!0, false)
-assert.equal(!!'', false)
-assert.equal(!!false, false, 'this is pure nonsense')
+assert.equal(!!undefined, false);
+assert.equal(!!null, false);
+assert.equal(!!NaN, false);
+assert.equal(!!0, false);
+assert.equal(!!'', false);
+assert.equal(!!false, false, 'this is pure nonsense');
 
 //------------------------------
 
 const anyGreaterThan10 = (input) => {
-  return input.some((value) => value > 10)
+  return input.some((value) => value > 10);
 }
 
 const allSameType = (input) => {
-  return input.every((value) => typeof (value) == typeof (input[0]))
+  return input.every((value) => typeof (value) == typeof (input[0]));
 }
 
-const removeNonVowels = (word) => word.replace(/[^aeiou]/gi, '')
+const removeNonVowels = (word) => word.replace(/[^aeiou]/gi, '');
 
-const onlyVowels = (input) => input.map(removeNonVowels)
+const onlyVowels = (input) => input.map(removeNonVowels);
 
-const wordUniqueCharacters = (word) => word.split('').filter((item, index, input) => input.indexOf(item) === index)
+const wordUniqueCharacters = (word) => word.split('').filter((item, index, input) => input.indexOf(item) === index);
 
 const allSameVowels = (input) => {
   return input.filter((word) => {
-    let vowels = removeNonVowels(word)
-    let uniqueVowels = wordUniqueCharacters(vowels)
-    return uniqueVowels.length == 1
+    let vowels = removeNonVowels(word);
+    let uniqueVowels = wordUniqueCharacters(vowels);
+    return uniqueVowels.length == 1;
   })
 }
-
 //------------------------------
-
 let goodInput = [8, 9, 10, 11];
 let badInput = [1, 2, 3, 4];
 
 assert.equal(anyGreaterThan10(goodInput), true);
 assert.equal(anyGreaterThan10(badInput), false);
-
 //---------------
-
 goodInput = [1, 2, 3];
 assert.equal(allSameType(goodInput), true);
 
 badInput = [1, 2, 3, '4'];
 assert.equal(allSameType(badInput), false);
-
 //---------------
-
 let input = ['awful', 'average', 'exceptional', 'amazing'];
-let expected = ['au', 'aeae', 'eeioa', 'aai']
-let actual = onlyVowels(input)
+let expected = ['au', 'aeae', 'eeioa', 'aai'];
+let actual = onlyVowels(input);
 
 assert.deepEqual(actual, expected);
-
 
 input = ['racecar', 'amalgam', 'oligopoly', 'zoom'];
-expected = ['amalgam', 'zoom']
-actual = allSameVowels(input)
+expected = ['amalgam', 'zoom'];
+actual = allSameVowels(input);
 
 assert.deepEqual(actual, expected);
 
-assert.equal(removeNonVowels("xaxexixoxuxa"), "aeioua")
+assert.equal(removeNonVowels('xaxexixoxuxa'), 'aeioua');
 
-assert.deepEqual(wordUniqueCharacters("xaxexixoxuxa"), ["x", "a", "e", "i", "o", "u"])
+assert.deepEqual(wordUniqueCharacters('xaxexixoxuxa'), ['x', 'a', 'e', 'i', 'o', 'u']);
