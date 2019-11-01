13a52d2d3f4813dd92dd1a495fea32ccc3856762
antoniopaolillo <antonio_francisco_p@live.com>
2019-11-01 16:17:08 -0300

    primeiro exercicio

--- src/zoo.js
+++ src/zoo.js
@@ -1,46 +1,64 @@
 const data = require('./data')
 
-function entryCalculator (entrants) {
+function entryCalculator(entrants = 0) {
+  let price = 0;
+
+  if (Object.keys(entrants).length === 0) {
+    price = 0
+  } else {
+    price = entrants.Adult * data.prices.Adult
+    price += entrants.Child * data.prices.Child
+    price += entrants.Senior * data.prices.Senior
+  }
+  return price
+};
+
+function schedule(dayName) {
+  const answer = () => data.hours.map((datas) => {
+    if (datas.close != 0) {
+      return `Open from ${open}am until ${close - 12}pm`
+    } else {
+      console.log(data.hours)
+      console.log(datas)
+      return `CLOSED`
+    }
+  })
+  return answer
+}
+
+function animalCount(species) {
+  console.log(data.animals[1].residents.length)
+};
+
+function animalMap(options) {
   // seu código aqui
 };
 
-function schedule (dayName) {
+function animalPopularity(rating) {
   // seu código aqui
 };
 
-function animalCount (species) {
+function animalsByIds(ids) {
   // seu código aqui
 };
 
-function animalMap (options) {
+function animalByName(animalName) {
   // seu código aqui
 };
 
-function animalPopularity (rating) {
+function employeesByIds(ids) {
   // seu código aqui
 };
 
-function animalsByIds (ids) {
+function employeeByName(employeeName) {
   // seu código aqui
 };
 
-function animalByName (animalName) {
+function managersForEmployee(idOrName) {
   // seu código aqui
 };
 
-function employeesByIds (ids) {
-  // seu código aqui
-};
-
-function employeeByName (employeeName) {
-  // seu código aqui
-};
-
-function managersForEmployee (idOrName) {
-  // seu código aqui
-};
-
-function employeeCoverage (idOrName) {
+function employeeCoverage(idOrName) {
   // seu código aqui
 };
 
