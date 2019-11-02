c6552c435cfe4ee4d4d4eafcb8d5037f26dd7f5a
Richard Wellerson <richardwellerson@gmail.com>
2019-11-02 16:59:38 -0300

    parte 4.1 feita

--- src/zoo.js
+++ src/zoo.js
@@ -1,6 +1,6 @@
 const data = require('./data')
 
-function entryCalculator (entrants) {
+function entryCalculator(entrants) {
   if (entrants == undefined || Object.keys(entrants).length == 0) {
     return 0
   }
@@ -9,52 +9,70 @@ function entryCalculator (entrants) {
   }, 0)
 };
 
-function schedule (dayName) {
+function schedule(dayName) {
   const schedule = data.hours
   if (dayName == undefined) {
     Object.keys(schedule).forEach((key) => schedule[key] = `Open from ${data.hours[key].open}am until ${data.hours[key].close - 12}pm`)
     schedule.Monday = "CLOSED"
-      return schedule
+    return schedule
   } else {
-     let day = {}
-     day[dayName] = schedule[dayName]
-     return day
+    let day = {}
+    day[dayName] = schedule[dayName]
+    return day
   }
 };
 
-function animalCount (species) {
+function animalCount(species = 0) {
+  const animals = data.animals, obj = {}
+  const isSpecie = (animal, species) => animal.name === species
+  const filterAnimals = (animals, species) => animals.filter(
+    (animal) => isSpecie(animal, species))
+  if (species == 0) {
+    Object.keys(animals).forEach((key) => {
+      obj[data.animals[key].name] = data.animals[key].residents.length
+    })
+  } else {
+    return filterAnimals(data.animals, species)[0].residents.length
+  }
+  return obj
+}
+
+function animalMap(options) {
+  const animals = data.animals, obj = {}
+  const isLocation = (animal, location) => animal.location === location
+  const filterAnimals = (animals, location) => animals.filter((animal) => isLocation(animal, location))
+  const getAllLocation = data.animals.map((animal)=>animal.location)
+  const locationReduce = getAllLocation.filter((este, i) => getAllLocation.indexOf(este) === i);
+  locationReduce.forEach(location => {
+    obj[location] = filterAnimals(animals,location).map(item => item.name)
+  })
+  return obj
+}
+function animalPopularity(rating) {
   // seu código aqui
 };
 
-function animalMap (options) {
+function animalsByIds(ids) {
   // seu código aqui
 };
 
-function animalPopularity (rating) {
+function animalByName(animalName) {
   // seu código aqui
 };
 
-function animalsByIds (ids) {
+function employeesByIds(ids) {
   // seu código aqui
 };
 
-function animalByName (animalName) {
+function employeeByName(employeeName) {
   // seu código aqui
 };
 
-function employeesByIds (ids) {
+function managersForEmployee(idOrName) {
   // seu código aqui
 };
 
-function employeeByName (employeeName) {
-  // seu código aqui
-};
-
-function managersForEmployee (idOrName) {
-  // seu código aqui
-};
-
-function employeeCoverage (idOrName) {
+function employeeCoverage(idOrName) {
   // seu código aqui
 };
 
