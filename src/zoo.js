79fed04a0ca7ce24d951e4d111153746c533bd22
Mateus Talles <mateustalles@gmail.com>
2019-11-05 16:08:51 -0300

    Código refatorado - code climate

--- src/zoo.js
+++ src/zoo.js
@@ -1,320 +1,249 @@
-  const data = require('./data')
+const data = require('./data')
 
-  function entryCalculator (entrants = 0){
-    if ( Object.keys(entrants).length === 0) {
-      return 0
-    }
-    return Object.keys(entrants).reduce( ( total, entrant )  => {
-      return total + (data.prices[entrant] * entrants[entrant])
-    }, 0)
-    
-  };
+function entryCalculator (entrants = 0) {
+  if (Object.keys(entrants).length === 0) {
+    return 0
+  }
+  return Object.keys(entrants).reduce( (total, entrant) => {
+    return total + (data.prices[entrant]*entrants[entrant])
+  }, 0)
+};
 
-  function schedule (dayName) {
-    const hours = data.hours
-    const hourKeys = Object.keys(data.hours)
-    let dayIndex = hourKeys.indexOf(dayName)
-    
-    function dailySchedule () {
-        for (let key of hourKeys) {
-        if (hours[key].open == 0 && hours[key].close == 0) {
-          hours[key] = 'CLOSED'
-        }
-        else {
-          hours[key] = `Open from ${hours[key].open}am until ${hours[key].close - 12}pm`
-        }
+function schedule (dayName) {
+  const hours = data.hours
+  const hourKeys = Object.keys(data.hours)
+  if (dayName == undefined) {
+    for (let key of hourKeys) {
+      if (hours[key].open == 0 && hours[key].close == 0) {
+        hours[key] = 'CLOSED'
+      }
+      else {
+        hours[key] = `Open from ${hours[key].open}am until ${hours[key].close - 12}pm`
       }
     }
+    return data.hours
+  } else {
+  return {[dayName] : data.hours[dayName]}
+  }
+};
 
-    if (dayName == undefined) {
-      dailySchedule();
-      return data.hours
-    }
-    
-    return {[dayName] : data.hours[dayName]}
-  };
-
-  function animalCount (species) {
-  // seu código aqui
-    if (species == undefined) {
-        const animals = data.animals
-        let animalsObject = {}
-        for (let animal of animals) {
-          animalsObject[animal.name] = animal.residents.length  
-        }
-        return animalsObject
+function animalCount (species) {
+  if (species == undefined) {
+      const animals = data.animals
+      let animalsObject = {}
+      for (let animal of animals) {
+        animalsObject[animal.name] = animal.residents.length  
       }
-
-      return data.animals.find( animal => animal.name == species).residents.length
-  };
-
-
-
+      return animalsObject
+    }
+    return data.animals.find( animal => animal.name == species).residents.length
+};
 
 function animalMap (options = {}) {
-  let animalObj = {}
   if (Object.keys(options).length === 0 ) {
-    function filterAnimals (region) {
-      const animalsRegion = data.animals.filter (animal => animal.location == region)
-      let regionalAnimals = []
-      for (let animal of animalsRegion) {
-        regionalAnimals.push(animal.name)
-      }
-      animalObj[region] = regionalAnimals
-      return animalObj
-    }
-    
-        filterAnimals("NE")
-        filterAnimals("NW")
-        filterAnimals("SE")
-        filterAnimals("SW")
-    
-        return animalObj
-      }
-  
-  function optionSort(param) {
-      param.sort()
+    let animalObj = {}
+    filterAnimals("NE", animalObj)
+    filterAnimals("NW", animalObj)
+    filterAnimals("SE", animalObj)
+    filterAnimals("SW", animalObj)
+    return animalObj
   }
-
-  function filterAnimalsResidents (region) {
-    let animalsRegion = data.animals.filter (beast => beast.location == region)
-    let residentAnimals = []
-    let animalsObjArray = []
-    for (let animal of animalsRegion) {
-      let animalResidents = animal.residents
-      if (options.hasOwnProperty('sex') === true) {
-        animalResidents = animal.residents.filter ( item => item.sex === options.sex)
-      }
-      for (let residents of animalResidents) {
-        residentAnimals.push(residents.name)
-      }
-      if (options.hasOwnProperty('sorted') === true) {
-        optionSort(residentAnimals)
-      }
-      let regionalAnimalsNames = {[animal.name] : residentAnimals}
-      animalsObjArray.push(regionalAnimalsNames)
-      regionalAnimalsNames = []
-      residentAnimals = []
+  function returnObject() {
+    const animalsNE = filterAnimalsResidents("NE", options)
+    const animalsNW = filterAnimalsResidents("NW", options)
+    const animalsSE = filterAnimalsResidents("SE", options)
+    const animalsSW = filterAnimalsResidents("SW", options)
+    const animalGroupings = Object.assign({}, animalsNE, animalsNW, animalsSE, animalsSW)
+    return animalGroupings
     }
-    
-  return  {[region] : animalsObjArray}
-}
-
-function returnObject() {
-  const animalsNE = filterAnimalsResidents("NE")
-  const animalsNW = filterAnimalsResidents("NW")
-  const animalsSE = filterAnimalsResidents("SE")
-  const animalsSW = filterAnimalsResidents("SW")
-  const animalGroupings = Object.assign({}, animalsNE, animalsNW, animalsSE, animalsSW)
-
-  return animalGroupings
-}
-
-if (options.hasOwnProperty('includeNames') === true) {
-    return returnObject()
-  }
-  else {
+    if (options.hasOwnProperty('includeNames') === true) {
+      return returnObject()
+    }
     const animalGroupings = returnObject()
     for (let key in animalGroupings) {
       for (let animal of animalGroupings[key]) {
-
         return { [key] : Object.keys(animal) }
-      }
+      }}
+}
+
+function filterAnimals (region, object) {
+  const animalsRegion = data.animals.filter (animal => animal.location == region)
+  let regionalAnimals = animalsRegion.reduce ((arr, animal) => {
+    arr.push(animal.name)
+    object[region] = arr
+    return arr
+  },[])
+  return regionalAnimals
+}
+
+function filterAnimalsResidents (region, options) {
+  const animalsRegion = data.animals.filter (beast => beast.location == region),
+    animalsObjArray = []
+  let residentAnimals = []
+  for (let animal of animalsRegion) {
+    let animalResidents = animal.residents
+    if (options.hasOwnProperty('sex') === true) {
+      animalResidents = animal.residents.filter ( item => item.sex === options.sex)
     }
+    for (let residents of animalResidents) {
+      residentAnimals.push(residents.name)
+    }
+    if (options.hasOwnProperty('sorted') === true) {
+      optionSort(residentAnimals)
+    }
+    let regionalAnimalsNames = {[animal.name] : residentAnimals}
+    animalsObjArray.push(regionalAnimalsNames)
+    regionalAnimalsNames = []
+    residentAnimals = []
+  }
+  return  {[region] : animalsObjArray}
+}
+
+function optionSort(param) {
+  param.sort()
+}
+
+function animalsByIds (...ids) {
+  if ( ids[0] == undefined  ) {
+    return ids
+  }
+  else {
+    let animalArr = []
+    for (let id of ids) {
+      animalArr.push (data.animals.find( item => item.id == id) )
+    }
+      return animalArr
+  }
+};
+
+function employeeByName (employeeName = 0) {
+  if (employeeName == 0) {
+    return {}
+  }
+  else {
+    const employees = data.employees
+    return employees.find( property => property.firstName == employeeName || property.lastName == employeeName )
+  }
+};
+
+function employeeCoverage (idOrName = "") {
+  const employees = data.employees,  coverageList = {}
+  let fullName, animalIds, animalArr = []
+  function getAnimalsArray(ids) {
+    for (let id of ids) {
+      animalArr.push (data.animals.find( item => item.id == id).name )
+    }
+  }
+  if (idOrName == "") {
+    for (let employee of employees) {
+      fullName = `${employee.firstName} ${employee.lastName}`
+      animalIds = employee.responsibleFor
+      getAnimalsArray(animalIds)
+      coverageList[fullName] = animalArr 
+      animalArr = []
+    }
+    return coverageList
+  }
+  else {
+    const employeeObj = employees.find (employee => employee.id == idOrName || employee.firstName == idOrName || employee.lastName == idOrName )
+    const animalIds = employeeObj.responsibleFor
+    getAnimalsArray(animalIds)
+    return { [`${employeeObj.firstName} ${employeeObj.lastName}`] : animalArr }
+  }
+};
+
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  let newEmployee = { id : id,
+    firstName: firstName,
+    lastName: lastName,
+    managers: managers,
+    responsibleFor: responsibleFor }
+  data.employees.push(newEmployee)
+}
+
+function isManager(id) {
+  const managerArray = data.employees.map( property => property.managers)
+  for (const arrays of managerArray) {
+    if (arrays.indexOf(id) === -1) {
+      return false
+    }
+    else {
+      return true
+    }}
+}
+
+function animalsOlderThan(animal, age) {
+  const selectedAnimal = data.animals.find( animals => animals.name == animal )
+  if (selectedAnimal.residents.every( resident => resident.age >= age)) {
+    return true
+  }
+  else {
+    return false
   }
 }
 
-  function animalPopularity (rating) {
-    // seu código aqui
-  };
+function oldestFromFirstSpecies(id) {
+  const employeeObj = data.employees.find (employee => employee.id == id)
+  const firstSpecieId = employeeObj.responsibleFor[0]
+  const animalObj = data.animals.find ( animal => animal.id == firstSpecieId)
+  const ageArray = animalObj.residents.map (element => element.age)
+  const oldestAnimal = animalObj.residents[ ageArray.indexOf(Math.max(...ageArray)) ]
+  const oldestAnimalArray = [ oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age ]
+  return oldestAnimalArray
+}
 
-  function animalsByIds (...ids) {
-    if ( ids[0] == undefined  ) {
-      // console.log("[]")
-      return ids
+function increasePrices(percentage) {
+  let prices = data.prices
+  function roundTo(n, digits) {
+    if (digits === undefined) {
+      digits = 0;
     }
-    else {
-      let animalArr = []
-      for (let id of ids) {
-        animalArr.push (data.animals.find( item => item.id == id) )
-      }
-        return animalArr
-    }
-  };
+    var multiplicator = Math.pow(10, digits);
+    n = parseFloat((n * multiplicator).toFixed(11));
+    var test =(Math.round(n) / multiplicator);
+    return +(test.toFixed(digits));
+  }
+  for (const key in prices) {
+    prices[key] = ( prices[key] * (100+percentage) / 100 )
+    prices[key] = roundTo(prices[key], 2)
+  }
+  return data.prices
+}
 
-  function animalByName (animalName) {
-    // seu código aqui
-  };
-
-  function employeesByIds (ids) {
-    // seu código aqui
-  };
-
-  function employeeByName (employeeName = 0) {
-    if (employeeName == 0) {
-      return {}
-    }
-    else {
-      const employees = data.employees
-      return employees.find( property => property.firstName == employeeName || property.lastName == employeeName )
-    }
-  };
-
-  function managersForEmployee (idOrName) {
-    // seu código aqui
-  };
-
-  function employeeCoverage (idOrName = "") {
-    const employees = data.employees
-    if (idOrName == "") {
-      let fullName
-      let animalArr = []
-      const coverageList = {}
-      let animalIds
-      for (let employee of employees) {
-        fullName = `${employee.firstName} ${employee.lastName}`
-        animalIds = employee.responsibleFor
-        for (let id of animalIds) {
-          animalArr.push (data.animals.find( item => item.id == id).name )
-        }
-        coverageList[fullName] = animalArr 
-        animalArr = []
-      }
-      return coverageList
-    }
-    else {
-      const employeeObj = employees.find (employee => employee.id == idOrName || employee.firstName == idOrName || employee.lastName == idOrName )
-      let fullName = `${employeeObj.firstName} ${employeeObj.lastName}`
-      let animalArr = []
-      let idedAnimal
-      const animalIds = employeeObj.responsibleFor
-      for (let id of animalIds) {
-        idedAnimal = data.animals.find( item => item.id == id)
-        animalArr.push(idedAnimal.name)
-      }
-      const employeeCoverage = {
-        [fullName] : animalArr
-      }
-      return employeeCoverage
-    }
-  };
-
-  function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
-    let newEmployee = { id : id,
-      firstName: firstName,
-      lastName: lastName,
-      managers: managers,
-      responsibleFor: responsibleFor }
-    data.employees.push(newEmployee)
+class Animal {
+  constructor ( name, age, sex, species){
+    this.name = name
+    this.age = age
+    this.sex = sex
+    this.species = species.slice(0, -1)
+    Animal.numInstances = (Animal.numInstances || 0) + 1;
   }
 
-  function isManager(id) {
-    const managerArray = data.employees.map( property => property.managers)
-    for (const arrays of managerArray) {
-      if (arrays.indexOf(id) === -1) {
-        return false
-      }
-      else {
-        return true
-      }
-    }
-   
+  info() {
+    const { name, age, sex, species } = this
+    return(`${name} is a ${age} year old ${sex} ${species}`)
   }
 
-  function animalsOlderThan(animal, age) {
-    const selectedAnimal = data.animals.find( animals => animals.name == animal )
-    if (selectedAnimal.residents.every( resident => resident.age >= age)) {
-      return true
-    }
-    else {
-      return false
-    }
+  static total_animals() { return Animal.numInstances }
+}
 
-  }
+function createAnimals() {
+  const animals = data.animals.reduce((arr, animal) => {
+    animal.residents.map(( {name, age, sex} ) => {
+      const newAnimal = new Animal(name, age, sex, animal.name) 
+      arr.push(newAnimal)
+    })
+    return arr
+  },[])  
+  return animals
+}
 
-  function oldestFromFirstSpecies(id) {
-    const employeeObj = data.employees.find (employee => employee.id == id)
-    const firstSpecieId = employeeObj.responsibleFor[0]
-    const animalObj = data.animals.find ( animal => animal.id == firstSpecieId)
-    const ageArray = animalObj.residents.map (element => element.age)
-    const oldestAnimal = animalObj.residents[ ageArray.indexOf(Math.max(...ageArray)) ]
-    const oldestAnimalArray = [ oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age ]
-    
-    return oldestAnimalArray
-    }
+const createEmployee = (personalInfo, associatedWith) => Object.assign({}, personalInfo, associatedWith)
 
-  function increasePrices(percentage) {
-    let prices = data.prices
-    function roundTo(n, digits) {
-      if (digits === undefined) {
-        digits = 0;
-      }
-      var multiplicator = Math.pow(10, digits);
-      n = parseFloat((n * multiplicator).toFixed(11));
-      var test =(Math.round(n) / multiplicator);
-      return +(test.toFixed(digits));
-    }
-
-    for (const key in prices) {
-      prices[key] = ( prices[key] * (100+percentage) / 100 )
-      prices[key] = roundTo(prices[key], 2)
-    }
-
-    return data.prices
-  }
-
-  class Animal {
-    constructor ( name, age, sex, species){
-      this.name = name
-      this.age = age
-      this.sex = sex
-      this.species = species.slice(0, -1)
-      Animal.numInstances = (Animal.numInstances || 0) + 1;
-    }
-
-    info() {
-      const { name, age, sex, species } = this
-      return(`${name} is a ${age} year old ${sex} ${species}`)
-    }
-
-    static total_animals() {
-      return Animal.numInstances
-    }
-  }
-
-  function createAnimals() {
-    const animals = data.animals.reduce((arr, animal) => {
-      animal.residents.map(( {name, age, sex} ) => {
-        const newAnimal = new Animal(name, age, sex, animal.name) 
-        arr.push(newAnimal)
-      })
-      return arr
-    },[])  
-    return animals
-  }
-
-  function createEmployee(personalInfo, associatedWith) {
-    return Object.assign({}, personalInfo, associatedWith)
-  }
-
-  module.exports = {
-    entryCalculator: entryCalculator,
-    schedule: schedule,
-    animalCount: animalCount,
-    animalMap: animalMap,
-    animalPopularity: animalPopularity,
-    animalsByIds: animalsByIds,
-    animalByName: animalByName,
-    employeesByIds: employeesByIds,
-    employeeByName: employeeByName,
-    managersForEmployee: managersForEmployee,
-    employeeCoverage: employeeCoverage,
-    addEmployee: addEmployee,
-    isManager: isManager,
-    animalsOlderThan: animalsOlderThan,
-    oldestFromFirstSpecies: oldestFromFirstSpecies,
-    increasePrices: increasePrices,
-    createAnimals: createAnimals,
-    Animal: Animal,
-    createEmployee: createEmployee
-  }
+module.exports = {  entryCalculator: entryCalculator,   schedule: schedule,
+  animalCount: animalCount,  animalMap: animalMap,   animalsByIds: animalsByIds,
+  employeeByName: employeeByName,  employeeCoverage: employeeCoverage,
+  addEmployee: addEmployee,  isManager: isManager,   animalsOlderThan: animalsOlderThan,
+  oldestFromFirstSpecies: oldestFromFirstSpecies,  increasePrices: increasePrices,
+  createAnimals: createAnimals,  Animal: Animal,  createEmployee: createEmployee
+}
